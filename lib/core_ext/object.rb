7258a254e41159eeb88e4abe092b0ea0395c0230
Paul Engel <paul.engel@holder.nl>
2012-06-11 00:57:46 +0200

    Added Object.detach_all and Object.detach_all!

--- lib/core_ext/object.rb
+++ lib/core_ext/object.rb
@@ -1,5 +1,13 @@
 class Object
 
+  def self.detach_all
+    Mass.index(self.class)[self.class.name].each{|object_id| Mass[object_id].detach}
+  end
+
+  def self.detach_all!
+    Mass.index(self.class)[self.class.name].each{|object_id| Mass[object_id].detach!}
+  end
+
   def _reference_instances(*mods)
     Mass.send(:instances_within, *mods)
   end
