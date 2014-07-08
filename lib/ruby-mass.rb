90fe935c62d67841abf5a69f13b69ecd40b19e64
jonathan schatz <jon@divisionbyzero.com>
2014-07-08 14:38:28 -0700

    fix requiring of core_ext when time-warp gem is already loaded

--- lib/ruby-mass.rb
+++ lib/ruby-mass.rb
@@ -1,3 +1,6 @@
-require "core_ext"
-require "mass"
-require "ruby-mass/version"
+# require 'core_ext' will fail if the time-warp gem is already loaded so we need
+# to require the fully qualified path name instead
+
+['core_ext', 'mass', 'ruby-mass/version'].each do |file|
+  require File.join File.dirname(__FILE__), file
+end
